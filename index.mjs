6446f83a097c407b067a279226b82d5fb976047d
tillery <richard.tillery@gmail.com>
2024-05-16 23:30:33 -0400

    [Richard Tillery] Work In Progress

--- index.mjs
+++ index.mjs
@@ -5,12 +5,14 @@ export const handler = async (event, context) => {
 
     console.log('Received event:', JSON.stringify(event, null, 2));
     let key = event.teamA + 'v' + event.teamB + '-' + event.teamA + '-' + event.date;
-    let value = event.line;
-    console.log('key = [%s] and value(line) = [%s]', key, value);
+    console.log('key = [%s] and value(line) = [%s]', key, event.line);
 
     var client = createClient(
         {
-        url:  `redis://localhost:6379`
+        url:  `redis://localhost:6379`,
+        socket: {
+            reconnectStrategy: false
+        }
         //url:  `redis://current-lines-ferywn.serverless.use1.cache.amazonaws.com:6379`
       }
     );
@@ -19,19 +21,30 @@ export const handler = async (event, context) => {
     });
 
     await client.connect();
+    const notInCache = await keyNotPresentInCache(key, client);
     
-    const [setKeyReply, fetchedValue] = await client
-    .multi()
-    .hSet(key, {
-        team: event.teamA,
-        line: value
-    })
-    .hGetAll(key)
-    .exec();
+    if( notInCache ) {
+        const [setKeyReply, fetchedValue] = await client
+        .multi()
+        .hSet(key, {
+            team: event.teamA,
+            line: event.line
+        })
+        .hGetAll(key)
+        .exec();
     
-    console.log(setKeyReply + '=' + JSON.stringify(fetchedValue, null, 2));
+        console.log('cache miss for key [%s], set in cache -> [%d]=[%s]', key, setKeyReply, JSON.stringify(fetchedValue, null, 5));
+    }
+    
 
     await client.disconnect();
 
     return event;  // Echo back the sent event
 };
+
+const keyNotPresentInCache = async function(key, client) {
+    const value = await client.hGetAll(key);
+    const keyNotPresent = Object.keys(value).length === 0;
+    console.log('checked cache for key [%s], key not present? = [%s]', key, keyNotPresent);
+    return keyNotPresent;
+}
